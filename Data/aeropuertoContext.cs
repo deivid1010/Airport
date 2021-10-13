8e7268c928d9ea24d8d7a7cd24aef659394428c6
Deivid <deividcastelm@gmail.com>
2021-10-13 17:11:31 -0500

    Agregar archivos de proyecto.

---
+++ Data/aeropuertoContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using aeropuerto.Model;
+
+namespace aeropuerto.Data
+{
+    public class aeropuertoContext : DbContext
+    {
+        public aeropuertoContext (DbContextOptions<aeropuertoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<aeropuerto.Model.Airport> Airport { get; set; }
+    }
+}
---
+++ Model/Airport.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace aeropuerto.Model
+{
+    public class Airport
+    {
+        public int id { get; set; }
+        public string ciudad { get; set; }
+        public string pais { get; set; }
+        public string region { get; set; }
+        public string postalZip { get; set; }
+        public string address { get; set; }
+    }
+}
